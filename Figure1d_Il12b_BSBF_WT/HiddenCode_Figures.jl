b5e8674333858d60b917dd3197edc8eb3e6e32cf
Irene Robles Rebollo <irenerobles93@gmail.com>
2021-11-17 14:05:24 +0000

    add Il12b_BF()

--- Figure1d_Il12b_BSBF_WT/HiddenCode_Figures.jl
+++ Figure1d_Il12b_BSBF_WT/HiddenCode_Figures.jl
@@ -39,3 +39,39 @@ function Il12b_maturefig(gen)
     
     PrettyPlotting.line075black()
 end
+
+
+function Il12b_BF(gen; pal = "Blues")
+    
+    gen = "WT"
+    s = MultipleTesting.adjust([do_mantelhaen(summary1, gen*"_0", gen*"_60")[1],
+        do_mantelhaen(summary1, gen*"_60", gen*"_90")[1],
+        do_mantelhaen(summary1, gen*"_90", gen*"_120")[1],
+        ], Bonferroni())
+
+    pd = Pandas.DataFrame(summary1[summary1[!,:Genotype].==gen, :])
+    Seaborn.boxplot(data = pd, x= "Timepoint", y = "BurstFraction", palette = pal, showfliers = false, )
+   # Seaborn.stripplot(data = pd, x= "Timepoint", y = "BurstFraction", hue = "Rep",palette = "Greys", size = 5,jitter = 0.1, zorder = 1)
+    pretty_axes2()
+
+    h = 0.05
+    u = 0.005
+
+    xy = [0, 1]
+    plt.plot(xy, [h, h], c = "black")
+    p =   round(s[1], sigdigits = 2)
+    plt.annotate("$p", xy = [Statistics.mean(xy), h + u], ha = "center", va = "center")
+    h = 0.1
+    xy = [1, 2]
+    plt.plot(xy, [h, h], c = "black")
+    p =   round(s[2], sigdigits = 2)
+    plt.annotate("$p", xy = [Statistics.mean(xy), h + u], ha = "center", va = "center")
+    h = 0.12
+    xy = [2, 3]
+    plt.plot(xy, [h, h], c = "black")
+    p =   round(s[3], sigdigits = 2)
+    plt.annotate("$p", xy = [Statistics.mean(xy), h + u], ha = "center", va = "center")
+    ylabel("Il12b \n Burst Frequency")
+    xlabel("Time after LPS (min)")
+     PrettyPlotting.line075black()
+end
