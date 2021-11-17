023891795e77781cc4b6bf3365fc142b77027493
Irene Robles Rebollo <irenerobles93@gmail.com>
2021-11-17 14:05:12 +0000

    add load modules and deal with problems when the timepoint is a string not a number

--- Figure1d_Il12b_BSBF_WT/HiddenCode_BSBF.jl
+++ Figure1d_Il12b_BSBF_WT/HiddenCode_BSBF.jl
@@ -1,3 +1,9 @@
+using Seaborn, NoLongerProblems_Pandas, Pandas, NoLongerProblems, PrettyPlotting, DataFrames, PyPlot
+using NoLongerProblems, Statistics
+using CSV, DataFrames, MultipleTesting
+using FQfiles, RCall, PyCall
+    
+
 function locusdata2(tab...; genotype = :Genotype, timepoint = true, 
         tss1 = :TSS1_r2, 
         tss2 = :TSS2_r2
@@ -39,7 +45,13 @@ function mean_burst_size_and_burst_fraction(df;
     new_df = DataFrames.DataFrame()
     new_df[!,:Sample] = unique(df[!,:Sample])
     new_df[!,:Genotype] = map(x-> split(x, "_")[1], new_df[!,:Sample])
-      new_df[!,:Timepoint] = map(x-> parse(Int, split(x, "_")[2]), new_df[!,:Sample])
+    
+    try
+        new_df[!,:Timepoint] = map(x-> parse(Int, split(x, "_")[2]), new_df[!,:Sample])
+    catch
+        new_df[!,:Timepoint] = map(x-> split(x, "_")[2], new_df[!,:Sample])
+    end
+
     
  function calculate_burst_size(sample; func = mean)
         d = completecases!(df[df[!,:Sample].==sample, :])
@@ -407,41 +419,6 @@ mantelhaen.test(Data.xtabs)
 return test[3][1],test[5][1]
 end
 
-
-function Il12b_BF(gen; pal = "Blues")
-    
-    gen = "WT"
-    s = MultipleTesting.adjust([do_mantelhaen(summary1, gen*"_0", gen*"_60")[1],
-        do_mantelhaen(summary1, gen*"_60", gen*"_90")[1],
-        do_mantelhaen(summary1, gen*"_90", gen*"_120")[1],
-        ], Bonferroni())
-
-    pd = Pandas.DataFrame(summary1[summary1[!,:Genotype].==gen, :])
-    Seaborn.boxplot(data = pd, x= "Timepoint", y = "BurstFraction", palette = pal, showfliers = false, )
-   # Seaborn.stripplot(data = pd, x= "Timepoint", y = "BurstFraction", hue = "Rep",palette = "Greys", size = 5,jitter = 0.1, zorder = 1)
-    pretty_axes2()
-
-    h = 0.05
-    u = 0.005
-
-    xy = [0, 1]
-    plt.plot(xy, [h, h], c = "black")
-    p =   round(s[1], sigdigits = 2)
-    plt.annotate("$p", xy = [Statistics.mean(xy), h + u], ha = "center", va = "center")
-    h = 0.1
-    xy = [1, 2]
-    plt.plot(xy, [h, h], c = "black")
-    p =   round(s[2], sigdigits = 2)
-    plt.annotate("$p", xy = [Statistics.mean(xy), h + u], ha = "center", va = "center")
-    h = 0.12
-    xy = [2, 3]
-    plt.plot(xy, [h, h], c = "black")
-    p =   round(s[3], sigdigits = 2)
-    plt.annotate("$p", xy = [Statistics.mean(xy), h + u], ha = "center", va = "center")
-    ylabel("Il12b \n Burst Frequency")
-    xlabel("Time after LPS (min)")
-     PrettyPlotting.line075black()
-end
 
 
 
