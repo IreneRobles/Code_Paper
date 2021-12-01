3cf9aefdac0b4bd972f9898bab8df7d61cad8ab2
Irene Robles Rebollo <irenerobles93@gmail.com>
2021-12-01 12:56:17 +0000

    fix bugs

--- CompleteSets/BD_inhibitors.jl
+++ CompleteSets/BD_inhibitors.jl
@@ -39,9 +39,7 @@ e1 = get_data(folder, gene, 1)
 e2 = get_data(folder, gene, 2)
 e3 = get_data(folder, gene, 3)
 e4 = get_data(folder, gene, 4)
-e5 = get_data(folder, gene, "5_30")
-e6 = get_data(folder, gene, "6_70")
-e7 = get_data(folder, gene, "7_47")
+
 exps = join_in_all_common_columns(e1,e2, e3, e4)
 CSV.write("GeneData/"*gene*".csv", exps)
 
