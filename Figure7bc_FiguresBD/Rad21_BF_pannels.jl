80c3b36912c47d10bfe305772192c5d8788b6a3b
Irene Robles Rebollo <irenerobles93@gmail.com>
2021-12-01 16:54:18 +0000

    fix colors

--- Figure7bc_FiguresBD/Rad21_BF_pannels.jl
+++ Figure7bc_FiguresBD/Rad21_BF_pannels.jl
@@ -1,39 +1,39 @@
 subplot(2,4,5)
-n = 1
+n = 0.2
 plot_BF_Genotype(BFs_all_gene_egr2enh,TEST_gene_egr2enh;
     hs = [0.4, 0.55, 0.63, 0.50].*n,
     ylimit = 0.7*n, 
     Genotype = "Rad21KO",
-    colorstripplot = "red",
-    colorboxplot = "darkgray",
+    colorstripplot = "darkred",
+    colorboxplot = "red",
     ylabel_ = "Enh. burst frequency")
 
 subplot(2,4,6)
-n = 1
+n = 0.2
 plot_BF_Genotype(BFs_all_gene_egr2,TEST_gene_egr2;
     hs = [0.4, 0.55, 0.63, 0.50].*n,
     ylimit = 0.7*n, 
     Genotype = "Rad21KO",
-    colorstripplot = "red",
-    colorboxplot = "darkgray",
+    colorstripplot = "darkred",
+    colorboxplot = "red",
     ylabel_ = "Gene burst frequency")
 
 subplot(2,4,7)
-n = 0.6
+n = 0.4
 plot_BF_Genotype(BFs_all_gene_hss1,TEST_gene_hss1;
     hs = [0.4, 0.55, 0.63, 0.50].*n,
     ylimit = 0.7*n, 
     Genotype = "Rad21KO",
-    colorstripplot = "red",
-    colorboxplot = "darkgray",
+    colorstripplot = "darkred",
+    colorboxplot = "red",
     ylabel_ = "Enh. burst frequency")
 
 subplot(2,4,8)
-n = 0.5
+n = 0.05
 plot_BF_Genotype(BFs_all_gene_il,TEST_gene_il;
     hs = [0.4, 0.55, 0.63, 0.50].*n,
     ylimit = 0.7*n, 
     Genotype = "Rad21KO",
-    colorstripplot = "red",
-    colorboxplot = "darkgray",
+    colorstripplot = "darkred",
+    colorboxplot = "red",
     ylabel_ = "Gene burst frequency")
