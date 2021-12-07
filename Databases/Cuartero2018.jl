fa10b03ed9f10657ad0bcf1ceb8ce9c9a2b7c5d3
Irene Robles Rebollo <irenerobles93@gmail.com>
2021-12-07 15:50:31 +0000

    fix paths, add functions

--- Databases/Cuartero2018.jl
+++ Databases/Cuartero2018.jl
@@ -68,7 +68,7 @@ end
 
 
 function Cuartero2018Deseq(name)
-    yifandeseqfolder = ENV["Code"]*"/../Code_Paper/Databases/Cuartero2018/DE_result_norm2Rep_n_Spikes/"
+    yifandeseqfolder = "../../Code_Paper/Databases/Cuartero2018/DE_result_norm2Rep_n_Spikes/"
     files = readdir(yifandeseqfolder)
      bool = [occursin(name, ii) for ii in files]
     file = files[bool]
@@ -80,6 +80,38 @@ function Cuartero2018Deseq(name)
         return files
     end
 end
+
+function Cuartero2018Deseq(;sample1 = "", sample2 = "", return_file = false)
+    folder = "../../Code_Paper/Databases/Cuartero2018/DE_result_norm2Rep_n_Spikes/"
+    try
+         bool1 =  [occursin(sample1, ii) for ii in readdir(folder)]
+        bool2 =  [occursin(sample2, ii) for ii in readdir(folder)]
+         bool = bool1.*bool2
+        
+         file = readdir(folder)[bool][1]
+
+        if return_file == true
+            return file
+        else
+            tb =  dropmissing(CSV.read(normpath(folder, file), DataFrame, missingstring = "NA"))
+            rename!(tb, Dict(
+                :ensembl_id => :EnsemblID,
+                :mgi_symbol => :GeneSymbol));
+            return tb
+        end
+            
+    catch
+        genotypes = [split(i, "_")[1] for i in readdir(folder)]
+        genotypes = append!(genotypes, [split(i, "_")[2] for i in readdir(folder)])
+        genotypes = unique(genotypes)
+        println("Samples available:")
+        for i in genotypes
+                println(i)
+        end
+   end
+end
+
+
 
 function GroseqDeseq(name)
     folder = ENV["Code"]*"/../Code_Paper/Databases/Cuartero2018/GROseq/"
@@ -95,5 +127,51 @@ function GroseqDeseq(name)
         return length(file)
     end
 end
+
+function get_DESEq_OTSUNIENH(sample1, sample2)
+    gro = CSV.read("../../Code_Paper/Databases/Cuartero2018/GROseq/GROseq_"*sample1*"__vs__"*sample2*"__DESeq2_OtsuniEnhancers.csv", DataFrames.DataFrame, missingstring = "NA")
+    gro[!,:coordinates] = string.(gro[!,:seqnames]) .* ":" .* string.(gro[!,:start]) .* "-" .* string.(gro[!,:end])
+    
+    enhancers = Cuartero2018.otsuni_enhancers()
+    enhancers[!,:coordinates] = "chr".*string.(enhancers[!,:chr]) .* ":" .* string.(enhancers[!,:start]) .* "-" .* string.(enhancers[!,:end])
+
+    gro = innerjoin(enhancers[!,[ "GeneSymbol_nearest", "EnhancerClass","Inter_Intragenic","coordinates","EnsemblID","chr"]], gro, on = :coordinates)
+    rename!(gro, :log2FoldChange => :log2FoldChange_Enh)
+    rename!(gro, :padj => :padj_Enh)
+    dropmissing(gro)
+end
+
+
+function otsuni_enhancers()
+    # Made on my first year of PhD
+    # Fixed on March 2019
+    file = string("../../Code_Paper/Databases/Cuartero2018/Enhancers.csv")
+     enhancers = CSV.read(file, DataFrames.DataFrame, skipto = 3)
+    rename!(enhancers, names(enhancers)[1] => :EnsemblID)
+    rename!(enhancers, :Column3 => :start)
+    rename!(enhancers, :Column4 => :end)
+    rename!(enhancers, :Column2 => :chr)
+    rename!(enhancers, :Column5 => :width)
+    rename!(enhancers, :Column6 => :strand)
+    rename!(enhancers, :Column7 => :EnhancerClass)
+    rename!(enhancers, :Column8 => :Inter_Intragenic)
+    rename!(enhancers, :Column9 => :GeneSymbol_nearest)
+
+    enhancers[!,:chr] = [try ii[4:end]; catch ii; end for ii in enhancers[!,:chr]]
+    enhancers[!,:feature] = ["enhancer" for ii in 1:nrow(enhancers)]
+
+    fea_name = []
+    count = 0
+    for fea in enhancers[!,:feature]
+        count += 1
+        countstring = "$count"
+        name_fea = string(fea, "_", countstring)
+        push!(fea_name, name_fea)
+    end
+    enhancers[!,:feature_name] = fea_name
+    return enhancers
+end
+
+
 
 end
