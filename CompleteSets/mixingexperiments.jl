e3ab1308753781c255f7d7c243d9b464c9cf5568
Irene Robles Rebollo <irenerobles93@gmail.com>
2021-12-17 11:49:03 +0000

    Remove mixed samples that where still there

--- CompleteSets/mixingexperiments.jl
+++ CompleteSets/mixingexperiments.jl
@@ -5,6 +5,9 @@ e1 = get_data(folder, gene, 1, addrep = 1)
 e2 = get_data(folder, gene, 2, addrep = 2)
 e3 = get_data(folder, gene, 3, addrep = 3)
 exps = vcat(e1,e2, e3)
+wt = exps[exps[!,:Genotype].=="WT", :]
+rad = exps[exps[!,:Genotype].=="Rad21KO", :]
+exps = vcat(wt, rad)
 CSV.write("GeneData/"*gene*".csv", exps)
 
 folder = "Cxcl10/"
@@ -16,4 +19,7 @@ e2 = get_data(folder, gene, 2, addrep = 2)
 e3 = get_data(folder, gene, 3, addrep = 3)
 e4 = get_data(folder, gene, 4, addrep = 4)
 exps = vcat(e1,e2, e3, e4)
+wt = exps[exps[!,:Genotype].=="WT", :]
+rad = exps[exps[!,:Genotype].=="Rad21KO", :]
+exps = vcat(wt, rad)
 CSV.write("GeneData/"*gene*".csv", exps)
