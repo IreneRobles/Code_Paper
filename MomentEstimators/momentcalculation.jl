044eb7d25218cfcc102e0d66f3928bb6e09b6c0f
Irene Robles Rebollo <irenerobles93@gmail.com>
2022-02-09 19:40:42 +0000

    add code to calculate BS BF from negative binomial

--- MomentEstimators/momentcalculation.jl
+++ MomentEstimators/momentcalculation.jl
@@ -81,3 +81,91 @@ function bootstrap_mean_std(genedata, gene; subsampling = true, cellssubsampled 
     new_df
     
 end
+
+
+using MATLAB
+using Distributions, DataFrames, CSV
+
+function nbinfit(array)
+    a = mxarray(Array(array))
+    mat"""
+    $dist = nbinfit($a)
+    """
+    r = dist[1]
+    p = dist[2]
+    
+    return NegativeBinomial(r, p)
+end
+
+function nbinfit_ci(array, alpha)
+    a = mxarray(Array(array))
+    mat"""
+    [$parmhat,$parmci] = nbinfit($a,$alpha) 
+    """
+    
+    dat = DataFrames.DataFrame()
+    dat[!,:parameters] = [:r, :p]
+    dat[!,:estimates] = parmhat[1, :]
+    dat[!,:ci_1] = parmci[1, :]
+    dat[!,:ci_2] = parmci[2, :]
+    dat
+
+end
+
+function nbinfit_burstsize(dat)
+    a = nbinfit_ci(dat, 0.01)
+    p = a[2,:estimates]
+    burst = (1-p)/p
+end
+
+function nbinfit_r(dat)
+    a = nbinfit_ci(dat, 0.01)
+    r = a[1,:estimates]
+    r
+end
+
+
+function BSBF_negativebinomial(genedata, gene; alpha = 0.01)
+    
+    #burst size = (1-p)/p
+    #burst frequency = r*mRNA lifetime
+    col = :Sample_Rep
+    if !in( "N_exon",names(genedata))
+        genedata[!,"N_exon"] = genedata[!,:TSS1_r2].+ genedata[!,:TSS2_r2] .+ genedata[!,:N_thres_Total]
+    end
+    new_df = DataFrames.DataFrame()
+    genedata[!,:Sample] = genedata[!,:Genotype] .* "_" .* string.(genedata[!,:Timepoint])
+    genedata[!,:Sample_Rep] = genedata[!,:Sample] .* "_" .* string.(genedata[!,:Rep])
+    samples = unique(genedata[!,col])
+    new_df[!,:Sample_Rep] = samples
+    spt = split_by(genedata, col)
+    ps = []
+    p_c1s = []
+    p_c2s = []
+    rs = []
+    r_c1s = []
+    r_c2s = []
+    for ii in samples
+        distribution = spt[ii][!,:N_exon]
+         dat = nbinfit_ci(distribution, alpha)
+        push!(rs, dat[1, :estimates])
+        push!(ps, dat[2, :estimates])
+        push!(r_c1s, dat[1, :ci_1])
+        push!(p_c1s, dat[2, :ci_1])
+        push!(r_c2s, dat[1, :ci_2])
+        push!(p_c2s, dat[2, :ci_2])
+    end
+    
+    new_df[!,:BF_NB] = rs
+    new_df[!,:BS_NB] = (1 .- ps)./ps
+    
+    
+    new_df[!,:r] = rs
+    new_df[!,:r_c1] = r_c1s
+    new_df[!,:r_c2] = r_c2s
+    new_df[!,:p] = ps
+    new_df[!,:p_c1] = p_c1s
+    new_df[!,:p_c2] = p_c2s 
+    new_df
+    
+end
